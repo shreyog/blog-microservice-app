08dbb16601011836a89088cf3f9dcf78e868a7c6
shreyog <shreyog23@gmail.com>
2020-11-12 22:46:33 +0530

    Added ingress service and skaffold support

--- query/index.js
+++ query/index.js
@@ -49,7 +49,7 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log(`🔥 Server Running on Port: 4002  🔥  `);
 
-  const res = await axios.get("http://event-bus-srv::4005/events");
+  const res = await axios.get("http://event-bus-srv:4005/events");
   for (let event of res.data) {
     console.log(`Processing event: ${event.type}`);
     handleEvent(event.type, event.data);
